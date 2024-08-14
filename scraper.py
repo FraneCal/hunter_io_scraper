1592e80a358d72d761c979116ffb0278c0681953
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2024-08-14 11:15:57 +0200

    updated the code so now it scrapes all of the necessary information

--- scraper.py
+++ scraper.py
@@ -4,8 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+import pandas as pd
 import time
-
 
 def setup_webdriver():
     driver = webdriver.Chrome()
@@ -14,13 +15,13 @@ def setup_webdriver():
 
 def login(email, password, URL, driver): 
     driver.get(URL)
-
+    
     email_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="email-field"]')))
     email_input.send_keys(email)
-
+    
     password_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="password-field"]')))
     password_input.send_keys(password)
-
+    
     log_in_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="signin_form"]/div[2]/button[2]')))
     log_in_button.click()
 
@@ -28,29 +29,75 @@ def search_company(company_domain):
     domain_search = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="domain-field"]')))
     domain_search.send_keys(company_domain)
     domain_search.send_keys(Keys.ENTER)
-
-    time.sleep(5)
-
-    page_source = driver.page_source
-
-    soup = BeautifulSoup(page_source, "html.parser")
-
-    results = soup.find('div', class_='ds-result__primary').getText()
-    print(results)
     
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'ds-result__data')))
+    
+    page_source = driver.page_source
+    
+    soup = BeautifulSoup(page_source, "html.parser")
+    boxes = soup.find_all('div', class_='ds-result__data')
+
+    results = []
+    
+    for box in boxes:
+        try:
+            full_name = box.find('div', class_='ds-result__fullname').getText()
+        except (NoSuchElementException, AttributeError):
+            full_name = None
+        
+        try:
+            email = box.find('div', class_='ds-result__email').getText()
+        except (NoSuchElementException, AttributeError):
+            email = None 
+        
+        try:
+            title = box.find('div', class_='ds-result__attribute').getText().strip()
+        except (NoSuchElementException, AttributeError):
+            title = None 
+        
+        try:
+            linkedin_link = box.find('a', {'aria-label': lambda value: value and 'LinkedIn' in value})
+            linkedin = linkedin_link.get('href') if linkedin_link else None
+        except (NoSuchElementException, AttributeError):
+            linkedin = None 
+        
+        try:
+            twitter_link = box.find('a', {'aria-label': lambda value: value and 'Twitter' in value})
+            twitter = twitter_link.get('href') if twitter_link else None
+        except (NoSuchElementException, AttributeError):
+            twitter = None 
+        
+        # Append to results list
+        results.append({
+            'Full Name': full_name,
+            'Email': email,
+            'Title': title,
+            'LinkedIn': linkedin,
+            'Twitter': twitter
+        })
+    
+    # Convert results to DataFrame
+    df = pd.DataFrame(results)
+    return df
+
 if __name__ == "__main__":
 
     URL = "https://hunter.io/search"
-
-    email = 'EMAIL'
-    password = 'PASSWORD'
-
+    
+    email = 'YOUR EMAIL'
+    password = 'YOUR PASSWORD'
+    
     driver = setup_webdriver()
-
+    
     login(email, password, URL, driver)
+    
+    company_domain = 'COMPANY DOMAIN YOU ARE SEARCHING FOR'
+    
+    results_df = search_company(company_domain)
+    
+    # Save results to Excel
+    results_df.to_excel('data.xlsx', index=False)
 
-    company_domain = 'COMPANY DOMAIN'
-
-    search_company(company_domain)
+    print("Scraping successful! Data has been saved to 'data.xlsx'.")
 
     driver.quit()
